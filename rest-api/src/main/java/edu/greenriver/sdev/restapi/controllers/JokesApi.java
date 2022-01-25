7295fa55067330b1c4ebd0f765b85e1d790bf6f6
Josh Archer <jarcher@greenriver.edu>
2022-01-25 14:52:28 -0800

    Turned on CORS settings in our API.

--- rest-api/src/main/java/edu/greenriver/sdev/restapi/controllers/JokesApi.java
+++ rest-api/src/main/java/edu/greenriver/sdev/restapi/controllers/JokesApi.java
@@ -14,6 +14,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("api/v1/jokes") //start all routes with /jokes
+@CrossOrigin(origins="*")
 public class JokesApi
 {
     private JokesService service;
